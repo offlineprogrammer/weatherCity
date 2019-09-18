73ce68fbd2e276f65c45fe7aef4d8f5bf7140da2
OfflineProgrammer <mmalaka@gmail.com>
2019-09-18 14:50:26 -0700

    storage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {  Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,25 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  async save(key: string, value: string): Promise < void > {
+    await Storage.set({
+      key,
+      value: JSON.stringify(value)
+    });
+  }
+
+  async remove(key: string): Promise < void > {
+    await Storage.remove({
+      key
+    });
+  }
+
+  async get(key: string): Promise < void > {
+    await Storage.get({
+      key
+    });
+  }
+
+
 }
