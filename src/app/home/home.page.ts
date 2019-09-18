6e24f5990e01fb1aa29439ecbc42b053a6ec9b03
OfflineProgrammer <mmalaka@gmail.com>
2019-09-18 13:14:45 -0700

    Geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
+const { Geolocation } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  async ngOnInit(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log(coordinates);
+  }
 
 }
