c0015734832f34cdbd393139baa6a6d1c72e8c28
OfflineProgrammer <mmalaka@gmail.com>
2019-09-19 13:04:58 -0700

    WeatherService

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@angular/core';
+import { WeatherResponse } from '../interfaces/weather';
+import { SettingsService } from '../services/settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  public data: WeatherResponse;
+  private apiKey = '';
+
+  constructor(private settingsService: SettingsService) { }
+
+  async load() {}
+
+  async refreshWeather() {}
+
+  processData (data: WeatherResponse, unit: string) {}
+
+  async getCurrentWeather() {}
 
   convertFromKelvin(amount: number, unit: string): number {
     if (unit === 'celsius'){
